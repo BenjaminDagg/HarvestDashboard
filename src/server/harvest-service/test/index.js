89439d6b61782753af7b1bd92ac82c1cab0bee89
adrfig966 <adrfigueroa@csumb.edu>
2018-04-01 23:26:17 -0700

    Started harvest service unit tests

---
+++ src/server/harvest-service/test/index.js
@@ -0,0 +1,205 @@
+/*
+ * Unit Tests for maps-service api
+ * 
+ * To run tests type 'mocha' in maps-service directory
+ */
+
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server');
+let should = chai.should();
+var request = require('supertest');
+
+chai.use(chaiHttp);
+
+const url = 'http://localhost:2000';
+
+const testUser = {
+		username: "test",
+		password: "password",
+		firstname: "firstname",
+		lastname: "lastname",
+		id: "5abc5cfb73db43393856e365",
+		createdAt: "2018-02-28"
+};
+
+
+var token = null;
+/*
+Testing maps-service
+*/
+describe('harvest-service', () => {
+	before((done) => {
+		chai.request(url)
+		.post('/authenticate')
+		.auth(testUser.username, testUser.password)
+		.end((err, res) => {
+			token = res.body.credentials.access_token;
+			console.log("token:" + token)
+			done();
+		});
+	});
+	describe('/GET /harvest/numcrates', () => {
+	
+		it('it should return Unauthorized if no bearer token is given in the authorization header' , (done) => {
+			chai.request(url)
+				.get('/harvest/numcrates?date=03312018')
+				.end((err, res) => {
+					res.should.have.status(401);
+					res.body.should.be.a('object');
+					res.body.should.have.property('error');
+					res.body.should.have.property('statusCode');
+					res.body.should.have.property('message');
+					done();
+				});
+		});
+		it('it should return Unauthorized if an invalid bearer token is given in the authorization header' , (done) => {
+			chai.request(url)
+				.get('/harvest/numcrates?date=03312018')
+				.set('authorization', 'Bearer' + "eyJhbGciOiJaUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVhYmM1Y2ZiNzNkYjQzMzkzODU2ZTM2NSIsImlhdCI6MTUyMjMwNDAxOCwiZXhwIjoxNTIyMzkwNDE4fQ.o6iXFxiTDUu_JWy4AVmasO7Ue6IIH6h5FZkCBCkhj0c")
+				.end((err, res) => {
+					res.should.have.status(401);
+					res.body.should.be.a('object');
+					res.body.should.have.property('error');
+					res.body.should.have.property('statusCode');
+					res.body.should.have.property('message');
+					done();
+				});
+		});
+		it('it should return an error as there is no date parameter' , (done) => {
+			chai.request(url)
+				.get('/harvest/numcrates')
+				.set('authorization', 'Bearer' + token)
+				.end((err, res) => {
+					res.should.have.status(400);
+					done();
+				});
+		});
+		it('it should return an error as there is an invalid date parameter' , (done) => {
+			chai.request(url)
+				.get('/harvest/numcrates?date=033120188')
+				.set('authorization', 'Bearer' + token)
+				.end((err, res) => {
+					res.should.have.status(400);
+					done();
+				});
+		});
+		it('it should return a list of all crates for 03/31/2018' , (done) => {
+			chai.request(url)
+				.get('/harvest/numcrates?date=03312018')
+				.set('authorization', 'Bearer' + token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.have.property('numCrates');
+					res.body.should.have.property('crates');
+					done();
+				});
+		});
+		
+	
+	});
+	
+	describe('/GET /harvest/meantime', () => {
+		it('it should return Unauthorized if no bearer token is given in the authorization header' , (done) => {
+			chai.request(url)
+				.get('/harvest/meantime?from=03302018&to=04012018')
+				.end((err, res) => {
+					res.should.have.status(401);
+					res.body.should.be.a('object');
+					res.body.should.have.property('error');
+					res.body.should.have.property('statusCode');
+					res.body.should.have.property('message');
+					done();
+				});
+		});
+		it('it should return Unauthorized if an invalid bearer token is given in the authorization header' , (done) => {
+			chai.request(url)
+				.get('/harvest/meantime?from=03302018&to=04012018')
+				.set('authorization', 'Bearer' + "eyJhbGciOiJaUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVhYmM1Y2ZiNzNkYjQzMzkzODU2ZTM2NSIsImlhdCI6MTUyMjMwNDAxOCwiZXhwIjoxNTIyMzkwNDE4fQ.o6iXFxiTDUu_JWy4AVmasO7Ue6IIH6h5FZkCBCkhj0c")
+				.end((err, res) => {
+					res.should.have.status(401);
+					res.body.should.be.a('object');
+					res.body.should.have.property('error');
+					res.body.should.have.property('statusCode');
+					res.body.should.have.property('message');
+					done();
+				});
+		});
+		it('it should return a the mean time for all crates since there is no parameters' , (done) => {
+			chai.request(url)
+				.get('/harvest/meantime')
+				.set('authorization', 'Bearer' + token)
+				.end((err, res) => {
+					res.body.should.have.property('meantime');
+					res.should.have.status(200);
+					done();
+				});
+		});
+		it('it should return a the mean time for all crates for crates from 03/31/2018 to 04/01/2018' , (done) => {
+			chai.request(url)
+				.get('/harvest/meantime?from=03302018&to=04012018')
+				.set('authorization', 'Bearer' + token)
+				.end((err, res) => {
+					res.body.should.have.property('meantime');
+					res.should.have.status(200);
+					done();
+				});
+		});
+	
+			
+	
+	});
+	describe('/GET /harvest/numcrates/between', () => {
+		it('it should return Unauthorized if no bearer token is given in the authorization header' , (done) => {
+			chai.request(url)
+				.get('/harvest/numcrates/between?from=03302018&to=04012018')
+				.end((err, res) => {
+					res.should.have.status(401);
+					res.body.should.be.a('object');
+					res.body.should.have.property('error');
+					res.body.should.have.property('statusCode');
+					res.body.should.have.property('message');
+					done();
+				});
+		});
+		it('it should return Unauthorized if an invalid bearer token is given in the authorization header' , (done) => {
+			chai.request(url)
+				.get('/harvest/numcrates/between?from=03302018&to=04012018')
+				.set('authorization', 'Bearer' + "eyJhbGciOiJaUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVhYmM1Y2ZiNzNkYjQzMzkzODU2ZTM2NSIsImlhdCI6MTUyMjMwNDAxOCwiZXhwIjoxNTIyMzkwNDE4fQ.o6iXFxiTDUu_JWy4AVmasO7Ue6IIH6h5FZkCBCkhj0c")
+				.end((err, res) => {
+					res.should.have.status(401);
+					res.body.should.be.a('object');
+					res.body.should.have.property('error');
+					res.body.should.have.property('statusCode');
+					res.body.should.have.property('message');
+					done();
+				});
+		});
+		it('it should return an error as there is no parameters' , (done) => {
+			chai.request(url)
+				.get('/harvest/numcrates/between')
+				.set('authorization', 'Bearer' + token)
+				.end((err, res) => {
+					res.should.have.status(400);
+					done();
+				});
+		});
+		it('it should return a list of all crates for crates from 03/31/2018 to 04/01/2018' , (done) => {
+			chai.request(url)
+				.get('/harvest/numcrates/between?from=03302018&to=04012018')
+				.set('authorization', 'Bearer' + token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.have.property('numCrates');
+					res.body.should.have.property('crates');
+					res.body.should.have.property('cratesPerDay');
+					done();
+				});
+		});
+	
+		
+	});
+	
+	
+});
